d8cda7b874676018030439ce4ae2883fd4c240ff
Vitali <vitali@Anastasiyas-MacBook-Air.local>
2024-01-25 12:20:11 +0100

    create button

---
+++ TinkoffCalculator/TinkoffCalculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Vitali on 25/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else {return}
+        print(buttonText)
+      
+    }
+    
+    
+}
+
